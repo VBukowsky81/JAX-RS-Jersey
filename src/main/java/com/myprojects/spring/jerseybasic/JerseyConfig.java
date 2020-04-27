3c888f1e919a01b5a250589a751e0e41cc9f44ee
Victor V. Bukowsky <54236067+VBukowsky81@users.noreply.github.com>
2020-04-26 20:35:42 -0700

    Add files via upload

---
+++ src/main/java/com/myprojects/spring/jerseybasic/JerseyConfig.java
@@ -0,0 +1,13 @@
+package com.myprojects.spring.jerseybasic;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+ 
+@Component
+public class JerseyConfig extends ResourceConfig 
+{
+    public JerseyConfig() 
+    {
+        register(UserResource.class);
+    }
+}
---
+++ src/main/java/com/myprojects/spring/jerseybasic/JerseybasicApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.spring.jerseybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseybasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JerseybasicApplication.class, args);
+    }
+
+}
